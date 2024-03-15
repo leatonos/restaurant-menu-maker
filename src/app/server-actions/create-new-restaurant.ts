47a7280e612e378cc66a081eaf01e5676cc6fbc7
Pedro Baptista <pedrofmattosb@gmail.com>
2024-03-15 18:58:04 -0400

    Little type fix

--- src/app/server-actions/create-new-restaurant.ts
+++ src/app/server-actions/create-new-restaurant.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { MongoClient, ObjectId } from 'mongodb';
-import { RestaurantMenu } from '../types/types';
+import { MenuStyle, RestaurantMenu } from '../types/types';
 
 
 // Replace these with your actual connection details
@@ -11,14 +11,27 @@ const collectionName = "Restaurants";
 import { NextRequest, NextResponse } from 'next/server'
  
 export async function createRestaurant(restaurantName:string, ownerId:string, restaurantAddress:string)  {
+
+  const defaultMenuStyle:MenuStyle= {
+    restaurantLogo: 'https://placehold.co/100',
+    backgroundColor: '#ffffff',
+    menuColor: '#4A1A1A',
+    subMenuColor: '#4A1A1A',
+    primaryColor: '#ffffff',
+    secondaryColor: '#ffffff',
+    fontColor: '#000000'
+  }
+
+
   const client = await MongoClient.connect(uri);
   const restaurantCollection = client.db(dbName).collection(collectionName)
   const newRestaurant:RestaurantMenu = {
-      _id: new ObjectId(),
-      ownerId: ownerId,
-      restaurantName: restaurantName,
-      restaurantAddress: restaurantAddress,
-      menuCategories: []
+    _id: new ObjectId(),
+    ownerId: ownerId,
+    restaurantName: restaurantName,
+    restaurantAddress: restaurantAddress,
+    menuCategories: [],
+    menuStyle: defaultMenuStyle
   } 
   try {
     
