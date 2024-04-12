7c5628106177bc85e42130394bc88292520084dd
Pedro Fernando de Mattos Baptista <pedrofmattosb@gmail.com>
2024-04-12 16:16:36 -0400

    Qrcodes added

--- src/app/components/user-components/restaurant-box.tsx
+++ src/app/components/user-components/restaurant-box.tsx
@@ -1,10 +1,11 @@
 'use client'
 
 import React, { useState } from 'react';
-import styles from '../../css/restaurant-box.module.css'
+import styles from '../../css/userpage.module.css'
 import { useUser } from '@auth0/nextjs-auth0/client';
 import { deleteRestaurant } from '@/app/server-actions/delete-restaurant';
 import Image from 'next/image';
+import QRCode from "react-qr-code";
 import EditImage from '../../../../public/edit.svg'
 import DeleteImage from '../../../../public/trash.svg'
 import PreviewImage from '../../../../public/preview.svg'
@@ -35,6 +36,15 @@ export default function RestaurantBox( props: MyProps ){
         !menuDeleted &&(
         <div className={styles.boxContainer}>
             <h2>{restaurantName}</h2>
+            <div style={{ height: "auto", margin: "0 auto", width: "65%" }}>
+                <QRCode
+                size={256}
+                bgColor={'none'}
+                style={{ height: "auto", maxWidth: "180px", width: "100%",padding:'5px' }}
+                value={`https://restaurant-menu-maker.vercel.app/restaurant/${restaurantId}`}
+                viewBox={`0 0 256 256`}
+                />
+            </div>
             <div className={styles.boxOptions}>
                 <button className={styles.boxButton}>
                     <a aria-label='Preview Restaurant' href={`restaurant/${restaurantId}`}>
