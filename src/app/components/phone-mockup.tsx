52544a70ce1a62ec3c03065cd30a20449de7f021
Pedro Fernando de Mattos Baptista <pedrofmattosb@gmail.com>
2024-09-10 03:23:29 -0400

    test

--- src/app/components/phone-mockup.tsx
+++ src/app/components/phone-mockup.tsx
@@ -1,20 +1,24 @@
-'use client'
 import styles from '../css/phone-mockup.module.css'
-import { useEffect, useRef } from 'react';
+import { Suspense } from 'react';
 
-interface MyProps {
-    menuLink:string
- }
+    interface MyProps {
+       menuLink:string
+    }
 export default async function PhoneMockup(props: MyProps) {
 
 
+
+    
+
     return (
        
-        <div className={styles.phone}>
-            <div className={styles.iframeContainer}>
-                <iframe src={props.menuLink}></iframe>
-            </div>
+        <div className="phone">
+        <div className="speaker"></div>
+        <div className="camera"></div>
+        <div className="iframe-container">
+          <iframe src={props.menuLink}></iframe>
         </div>
+      </div>
        
         )
   }
