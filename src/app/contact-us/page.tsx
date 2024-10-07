cdf27cb3e02d8c41536919d478b5742ba9579598
Pedro Fernando de Mattos Baptista <pedrofmattosb@gmail.com>
2024-10-07 00:10:12 -0400

    Small import path fix

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,14 +1,12 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "../page.module.css";
 import { useUser } from '@auth0/nextjs-auth0/client';
-import ProfileClient from "./components/user-components/userInfo";
 import { getSession } from "@auth0/nextjs-auth0";
 import { redirect } from  'next/navigation';
 import HomeHeader from "../components/home-page-header";
 
 //Images imports
-import MainLogo from "../../public/logos/Menu Factory - Horizontal.svg"
-import AppImage from "../../public/about-images/app-image-one.svg"
+import MainLogo from "../../../public/logos/Menu Factory - Horizontal.svg"
 
 //Lottie Import
 import PhoneLottieAnimation from "../../../public/lottie-animations/phone-animation-component";
